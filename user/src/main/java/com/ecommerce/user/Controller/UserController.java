f96f1584a421b103db8a66ace86e0a419b78ff02
ujjwal kumar <ujjwalkumararch@gmail.com>
2026-03-22 14:53:25 +0100

    implement logger for controller, Grafana implemented

--- user/src/main/java/com/ecommerce/user/Controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/Controller/UserController.java
@@ -4,6 +4,8 @@ import com.ecommerce.user.DTO.UserRequest;
 import com.ecommerce.user.DTO.UserResponse;
 import com.ecommerce.user.Service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,8 @@ public class UserController {
     private final UserService userService;
     private Object user;
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
 
     @GetMapping(value = "/users/all", headers = "content-type=text/text", produces = "text/plain")
     public ResponseEntity<List<UserResponse>> getAllUsers()
@@ -29,6 +33,7 @@ public class UserController {
     @GetMapping("/public/users/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id)
     {
+        logger.info("Request received for user: {}"+ id);
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
